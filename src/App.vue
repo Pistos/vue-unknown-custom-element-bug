9243109467e8c3188d3aac47b1fe51b77fbb79bf
Pistos <git dawt pistos aet purepistos dawt net>
2017-11-22 16:46:52 -0500

    Show how to work around the bug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
+  <!-- ************************************************************** -->
+  <!-- Delete the three lines indicated below to work around the bug: -->
+  <!-- ************************************************************** -->
+
   <div id="app">
+
+    <!-- 1. delete this line -->
     <nav-bar/>
+
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+
+// 2. delete this line
 import NavBar from '@/components/NavBar'
 
 export default {
   components: {
+    // 3. delete this line
     NavBar,
   },
   name: 'app',
